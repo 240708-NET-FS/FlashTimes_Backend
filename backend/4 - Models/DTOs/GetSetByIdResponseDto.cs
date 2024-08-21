a29d40434447550d1a747566528041d87a674c8d
“Owusu <owusuachamfour@gmail.com>
2024-08-21 14:44:32 -0400

    Handled possible null reference warnings and cleaned up GetSetByIdResponseDto by removing unnecessary UserId property.

--- backend/4 - Models/DTOs/GetSetByIdResponseDto.cs
+++ backend/4 - Models/DTOs/GetSetByIdResponseDto.cs
@@ -6,7 +6,6 @@ public class GetSetByIdResponseDto
     public int SetId { get; set; }
     public string? SetName { get; set; }
     public int SetLength { get; set; }
-    public int UserId { get; set; }
-    public UserDto Author { get; set; }
-    public List<FlashcardDto> Flashcards { get; set; }
+    public UserDto? Author { get; set; }
+    public List<FlashcardDto>? Flashcards { get; set; }
 }
--- backend/4 - Models/DTOs/UserDto.cs
+++ backend/4 - Models/DTOs/UserDto.cs
@@ -6,8 +6,8 @@ namespace FlashTimes.Models.DTOs;
 public class UserDto
 {
     public int UserId { get; set; }
-    public string UserName { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? UserName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public DateTime CreatedAt { get; set; }
 }
